b4caf05a882e59afb872d1a4163fec0bd3b4204c
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-03-03 02:46:02 +0000

    [create]3-13投稿機能実装完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,19 @@ class BooksController < ApplicationController
   end
 
   def index
+    #Viewへ渡すためのインスタンス変数に全データのオブジェクトを作成する。
+    @books=Book.all
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @book=Book.new
+  end
+
+ def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # 4.インデックス画面へリダイレクト
+    redirect_to '/books/show'
   end
 
   def show
@@ -10,4 +23,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+ private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get '/'  => 'homes#top'
-  get '/books' => 'books#new', as:'books'
-  get 'books/index'
-  get 'books/show'
+  get 'books/new'
+  get '/books' => 'books#index', as:'books'
+  post 'books' => 'books#create'
+  get '/books/show'
   get 'books/edit'
+
   resources :reviews
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
